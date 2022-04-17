3d768ff870665e4ceb683931eb2f7bcf53f3704e
Tarin <77469919+T4R1N@users.noreply.github.com>
2022-04-17 18:28:33 -0500

    Add files via upload

--- #Celly mk-IV.py
+++ #Celly mk-IV.py
@@ -16,6 +16,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord. Activity(type=discord.ActivityType.watching, name='you'))
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
@@ -31,21 +32,30 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # if message.channel.name == 'general':
-    #     if user_message.lower() == 'hello':
-    #         await message.channel.send(f'Hello, {username}!')
-    #         return
-    #     elif user_message.lower() == 'bye':
-    #         await message.channel.send(f'Goodbye, {username}!')
-    #         return
-    #     elif user_message.lower() == '!random':
-    #         response = f'This is your random number: {random.randrange(1000000)}'
-    #         await message.channel.send(response)
-    #         return
 
-    # if user_message.lower() == '!anywhere':
-    #     await message.channel.send('This can be used anywhere!')
-    #     return
+    #Hey Celly,
+    if 'heycelly' in user_message_nospace.lower() or 'hc' in user_message_nospace.lower():
+        if 'help' in user_message_nospace.lower():
+            await message.channel.send("Say 'Hey Celly' in order to begin a command.\n`Rickroll` -> does not rickroll you no matter what you do to it.\n`Charity` -> spits out a random charity to donate to.\n\nThese commands don't need 'Hey Celly'.\n\n`fart` -> Don't say this. It will subtract from the Puffhyu Score.\n`℗` -> Displays the current Puffhyu Score.\n`I love Puffhyu` -> Increases Puffhyu Score.\n`I hate Puffhyu` -> Decreases Puffhyu Score.")
+            return
+        if 'rickroll' in user_message_nospace.lower():
+            if "don't" in user_message_nospace.lower() or "do not" in user_message_nospace.lower():
+                await message.channel.send('If you say so.')
+                return
+            else:
+                await message.channel.send('No.')
+                return
+        if 'charity' in user_message_nospace.lower():
+            if "don't" in user_message_nospace.lower() or "do not" in user_message_nospace.lower():
+                await message.channel.send("Fine. Be that way.") 
+                return
+            else:
+                charities = ['https://www.stjude.org/donate/pm.html?sc_dcm=58700006946881490&sc_cid=kwp&sc_cat=b&&amount=x&msclkid=2e4dc908262714e0a38dab54b85c94d1&utm_source=bing&utm_medium=cpc&utm_campaign=M%20-%20Core%20Brand%20EX%20EL&utm_term=st%20jude%20donate&utm_content=PM_PR_Dig_Srch_PM_AO_US_Nat_MS_M-Ads_LF_-_-_-_SERP-Exct_Srch-B_-_-_-_Donate_-_EL_-_-_Srch&gclid=2e4dc908262714e0a38dab54b85c94d1&gclsrc=3p.ds','https://www.mercycorps.org/donate/crisis-ukraine-give-now','https://www.feedingamerica.org/ways-to-give']
+                await message.channel.send(random.charities)
+                return
+          
+
+    #Puffhyu Score
 
     if 'fart' in user_message_nospace.lower():
         await message.channel.send(f'{fulluser} has been reported to Discord Staff for saying {user_message}. (Message contains the f-word). -1℗.')
